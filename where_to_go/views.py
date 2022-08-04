f2b54d2debf43ee37a1ead4bbc17093bdc528aec
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-08-04 11:01:42 +0300

    Create endpoint API for places

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from places.models import Place
+from django.http import Http404
+from django.http.response import JsonResponse
+from django.shortcuts import get_list_or_404, render
+from places.models import Image, Place
 
 moscow_legends = Place.objects.get(placeId='moscow_legends')
 roofs24 = Place.objects.get(placeId='roofs24')
 
-places = {
+places_geojson = {
     "type": "FeatureCollection",
     "features": [
         {
@@ -40,4 +42,28 @@ places = {
 def show_start_page(request):
     return render(request,
                   'index.html',
-                  {'places_geojson': places})
+                  {'places_geojson': places_geojson})
+
+
+def get_place_json(place):
+    images = get_list_or_404(Image, place=place)
+    place_json = {
+        "title": place.title,
+        'img': [item.image.url for item in images],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.longitude,
+            'lat': place.latitude
+        }
+    }
+    return place_json
+
+
+def places(request, post_id):
+    try:
+        place = Place.objects.get(pk=post_id)
+    except Place.DoesNotExist:
+        raise Http404("No MyModel matches the given query.")
+    return JsonResponse(get_place_json(place), safe=False,
+                        json_dumps_params={'ensure_ascii': False, 'indent': 2})
