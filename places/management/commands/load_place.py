d988c405690549513444c9f92e835828eed29c51
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-08-10 20:39:40 +0300

    Fix multiple url handling

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -21,20 +21,21 @@ class Command(BaseCommand):
             response.raise_for_status()
             place_details = response.json()
 
-        place, _ = Place.objects.get_or_create(
-            title=place_details['title'],
-            defaults={
-                'description_short': place_details['description_short'],
-                'description_long': place_details['description_long'],
-                'longitude': place_details['coordinates']['lng'],
-                'latitude': place_details['coordinates']['lat']
-            }
-        )
+            place, _ = Place.objects.get_or_create(
+                title=place_details['title'],
+                defaults={
+                    'description_short': place_details['description_short'],
+                    'description_long': place_details['description_long'],
+                    'longitude': place_details['coordinates']['lng'],
+                    'latitude': place_details['coordinates']['lat']
+                }
+            )
 
-        for image_url in place_details['imgs']:
-            response = requests.get(image_url)
-            response.raise_for_status()
-            content = ContentFile(response.content)
-            image_name = Path(urlparse(image_url).path).name
-            new_image = Image(place=place)
-            new_image.image.save(image_name, content, save=True)
+            for image_url in place_details['imgs']:
+                response = requests.get(image_url)
+                response.raise_for_status()
+                content = ContentFile(
+                    content=response.content,
+                    name=Path(urlparse(image_url).path).name)
+                image = Image(place=place, image=content)
+                image.save()
